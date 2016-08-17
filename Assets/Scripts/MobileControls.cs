abec6a59a1e7d490b6bc0dfa4a6fc3aeb0d0cfec
Alan S <alstew13@hotmail.com>
2016-08-17 23:21:53 +0200

    CrossPlatform Assets

---
+++ Assets/Scripts/MobileControls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileControls : MonoBehaviour
+{
+	public float minSwipeDistY;
+
+	public float minSwipeDistX;
+
+	private Vector2 startPos;
+
+	void Update()
+	{
+		if (Input.touchCount > 0)
+		{
+
+			Touch touch = Input.touches [0];
+
+		}
+			
+	}
+}
