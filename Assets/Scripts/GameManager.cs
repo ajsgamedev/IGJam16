9f2349783400ceeeaf0974787ae3dbc99be4c68c
Merlin Stollenwerk <merlin-stollenwerk@past-development.de>
2016-08-17 19:41:29 +0200

    - worked on movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+    public float tileWidth;
+    public List<Transform> tilesInUse;
+    public List<Transform> tilePool;
+
+    Vector2 castingPoint;
+	// Use this for initialization
+	void Start () {
+        castingPoint.x = 0;
+        castingPoint.y = Screen.height;
+        //horzExtent = Camera.main.orthographicSize * Screen.width / Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float runningSpeed;
+    public MaskType currentMask;
+    SpriteRenderer renderer;
+	// Use this for initialization
+	void Start () {
+        currentMask = MaskType.Crow;
+        renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Input
+	    if(Input.GetKeyDown(KeyCode.A))
+        {
+            SwipeMask(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            SwipeMask(true);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ActivateMask();
+        }
+        //Movement
+        transform.Translate(runningSpeed * Time.deltaTime, 0, 0);
+	}
+
+    void ActivateMask()
+    {
+        
+    }
+
+    void SwipeMask(bool direction)
+    {
+        if(direction)
+        {
+            currentMask += 1;
+            if((int)currentMask >= 3)
+            {
+                currentMask = (MaskType)0;
+            }
+        }
+        else
+        {
+            currentMask -= 1;
+            if(currentMask < 0)
+            {
+                currentMask = (MaskType)2;
+            }
+        }
+        switch(currentMask)
+        {
+            case MaskType.Crow:
+                renderer.material.color = Color.blue;
+                break;
+            case MaskType.Bull:
+                renderer.material.color = Color.green;
+                break;
+            case MaskType.Mice:
+                renderer.material.color = Color.grey;
+                break;
+        }
+    }
+
+    
+}
+
+public enum MaskType
+{
+    Crow,
+    Bull,
+    Mice
+}
