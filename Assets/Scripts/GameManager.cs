63a135b2ae2e042e0abda8d23649faa521066276
Merlin Stollenwerk <merlin-stollenwerk@past-development.de>
2016-08-18 14:38:00 +0200

    - added new object pools

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,19 @@ public class GameManager : MonoBehaviour {
 	public GameObject[] UIHearts;
 
 	public float tileWidth;
+    public float minimumFreeSpace;
+    float freeSpaceCounter;
     public List<Transform> tilesInUse;
     public List<Transform> tilePool;
 
     public List<Transform> obstaclesInUse;
     public List<Transform> obstaclePool;
+
+    public List<Transform> normalBirdsInUse;
+    public List<Transform> normalBirdPool;
+
+    public List<Transform> bigBirdsInUse;
+    public List<Transform> bigBirdPool;
 
     float movedDistance = 0;
     Vector3 lastPos;
@@ -20,12 +28,20 @@ public class GameManager : MonoBehaviour {
     public static GameManager instance;
 
     //ObstacleSpawnStuff
-    public int nextSpawnInterval;
-    int currentSpawnInterval;
+    public int nextObstacleSpawnInterval;
+    int currentObstacleSpawnInterval;
+
+    //SmallBirdSpawnStuff
+    public int nextBirdSpawnInterval;
+    int currentBirdSpawnInterval;
+
+    //BigBirdSpawnStuff
+    public int nextBigBirdSpawnInterval;
+    int currentBigBirdSpawnInterval;
 
 	// Use this for initialization
 	void Start () {
-        currentSpawnInterval = nextSpawnInterval;
+        currentObstacleSpawnInterval = nextObstacleSpawnInterval;
         SetupTiles();
         instance = this;
 	}
@@ -50,8 +66,12 @@ public class GameManager : MonoBehaviour {
         tilePool[randomNumber].localPosition = lastPos;
         tilesInUse.Add(tilePool[randomNumber]);
         tilePool.RemoveAt(randomNumber);
-        currentSpawnInterval--;
-        if(currentSpawnInterval <= 0)
+        currentObstacleSpawnInterval--;
+        currentBirdSpawnInterval--;
+        currentBigBirdSpawnInterval--;
+        freeSpaceCounter--;
+
+        if(freeSpaceCounter <= 0 && currentObstacleSpawnInterval <= 0)
         {
             int randomNumber2 = Random.Range(0, obstaclePool.Count);
             obstaclePool[randomNumber2].localPosition = lastPos;
@@ -64,8 +84,44 @@ public class GameManager : MonoBehaviour {
                 obstaclePool.Add(obstaclesInUse[0]);
                 obstaclesInUse.RemoveAt(0);
             }
-            currentSpawnInterval = nextSpawnInterval;
+            currentObstacleSpawnInterval += nextObstacleSpawnInterval;
+            freeSpaceCounter = minimumFreeSpace;
         }
+
+        if (freeSpaceCounter <= 0 && currentBigBirdSpawnInterval <= 0)
+        {
+            int randomNumber2 = Random.Range(0, bigBirdPool.Count);
+            bigBirdPool[randomNumber2].localPosition = lastPos;
+            bigBirdsInUse.Add(bigBirdPool[randomNumber2]);
+            bigBirdPool.RemoveAt(randomNumber2);
+
+            //Just to ensure the removing happens late enough
+            if (bigBirdsInUse.Count > 3)
+            {
+                bigBirdPool.Add(bigBirdsInUse[0]);
+                bigBirdsInUse.RemoveAt(0);
+            }
+            currentBigBirdSpawnInterval += nextBigBirdSpawnInterval;
+            freeSpaceCounter = minimumFreeSpace;
+        }
+
+        if (freeSpaceCounter <= 0 && currentBirdSpawnInterval <= 0)
+        {
+            int randomNumber2 = Random.Range(0, normalBirdPool.Count);
+            normalBirdPool[randomNumber2].localPosition = lastPos;
+            normalBirdsInUse.Add(normalBirdPool[randomNumber2]);
+            normalBirdPool.RemoveAt(randomNumber2);
+
+            //Just to ensure the removing happens late enough
+            if (normalBirdsInUse.Count > 3)
+            {
+                normalBirdPool.Add(normalBirdsInUse[0]);
+                normalBirdsInUse.RemoveAt(0);
+            }
+            currentBirdSpawnInterval += nextBirdSpawnInterval;
+            freeSpaceCounter = minimumFreeSpace;
+        }
+
     }
 
 	// Update is called once per frame
