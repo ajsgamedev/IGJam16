e840e55cb6c9399a292ee05fd0862ad19c9a5327
Alan S <alstew13@hotmail.com>
2016-08-18 17:43:07 +0200

    hjkl;'

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,7 +25,6 @@ public class Player : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		currentMask = MaskType.Crow;
 		renderer = GetComponent<SpriteRenderer> ();
 
 		runningSpeed = speed;
@@ -44,87 +43,13 @@ public class Player : MonoBehaviour
 		}
 	}
 
-	IEnumerable ActivateMask ()
-	{
-		maskActivated = true;
-		switch (currentMask)
-		{
-		case MaskType.Crow:
-
-			break;
-		case MaskType.Bull:
-
-			break;
-		case MaskType.Mice:
-
-			break;
-		}
-		yield return new WaitForSeconds (maskTime);
-		maskActivated = false;
-	}
-
-	void SwipeMask (bool direction)
-	{
-		if (direction)
-		{
-			currentMask += 1;
-			if ((int)currentMask >= 3)
-			{
-				currentMask = (MaskType)0;
-			}
-		}
-		else
-		{
-			currentMask -= 1;
-			if (currentMask < 0)
-			{
-				currentMask = (MaskType)2;
-			}
-		}
-		switch (currentMask)
-		{
-		case MaskType.Crow:
-			renderer.material.color = Color.blue;
-			break;
-		case MaskType.Bull:
-			renderer.material.color = Color.green;
-			break;
-		case MaskType.Mice:
-			renderer.material.color = Color.grey;
-			break;
-		}
-	}
-
-	public void OnTriggerStay (Collider other)
-	{
-        
-		if (other.name == "Enemy1" || other.name == "Enemy3")
-		{
-			GameManager.instance.Lose ();
-		}
-		if (other.name == "Enemy2" && !(maskActivated && currentMask == MaskType.Bull))
-		{
-			GameManager.instance.Lose ();
-		}
-	}
-
-	public void Jump()
-	{
-		//rigidb.velocity = new Vector2 (0, jumpForce);
-	}
-
-	public void Slide ()
-	{
-
-	}
-
 }
 
 
 
 public enum MaskType
 {
-	Crow,
-	Bull,
-	Mice
+	BlueBird,
+	GreenBird,
+	YellowBird
 }
