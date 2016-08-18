c3b18b915fadbfa1c7d95c8cf8f4e9086c4a7758
Alan S <alstew13@hotmail.com>
2016-08-18 02:07:19 +0200

    Build and Swipe
    Andriod build and swipe wroking

--- Assets/Scripts/MobileControls.cs
+++ Assets/Scripts/MobileControls.cs
@@ -1,21 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
 
 public class MobileControls : MonoBehaviour
 {
-	public float minSwipeDistY;
+	public Text swipe;
+	private Vector2 fingerStart;
+	private Vector2 fingerEnd;
 
-	public float minSwipeDistX;
+	public enum Movement
+	{
+		Left,
+		Right
+	};
 
-	private Vector2 startPos;
+	public List<Movement> movements = new List<Movement>();
 
 	void Update()
 	{
-		if (Input.touchCount > 0)
+		//Example usage in Update. Note how I use Input.GetMouseButton instead of Input.touch
+
+		//GetMouseButtonDown(0) instead of TouchPhase.Began
+		if (Input.GetMouseButtonDown(0)) {
+			fingerStart = Input.mousePosition;
+			fingerEnd  = Input.mousePosition;
+		}
+
+		//GetMouseButton instead of TouchPhase.Moved
+		//This returns true if the LMB is held down in standalone OR
+		//there is a single finger touch on a mobile device
+		if(Input.GetMouseButton(0)) {
+			fingerEnd = Input.mousePosition;
+
+			//There was some movement! The tolerance variable is to detect some useful movement
+			//i.e. an actual swipe rather than some jitter. This is the same as the value of 80
+			//you used in your original code.
+			if(Mathf.Abs(fingerEnd.x - fingerStart.x) > 80)
+			{
+
+				//There is more movement on the X axis than the Y axis
+				if(Mathf.Abs(fingerStart.x - fingerEnd.x) > Mathf.Abs(fingerStart.y - fingerEnd.y)) 
+				{
+					
+					if ((fingerEnd.x - fingerStart.x) > 0)
+					{
+						//Right Swipe
+						movements.Add (Movement.Right);
+						swipe.text = "Swipe: Right!!";
+					}
+					else
+					{
+						//Left Swipe
+						movements.Add (Movement.Left);
+						swipe.text = "Swipe: Left!!";
+
+					}
+				}
+
+				//After the checks are performed, set the fingerStart & fingerEnd to be the same
+				fingerStart = fingerEnd;
+
+				//Now let's check if the Movement pattern is what we want
+				//In this example, I'm checking whether the pattern is Left, then Right, then Left again
+				//Debug.Log (CheckForPatternMove(0, 3, new List<Movement>() { Movement.Left, Movement.Right, Movement.Left } ));
+			}
+		}
+
+		//GetMouseButtonUp(0) instead of TouchPhase.Ended
+		if(Input.GetMouseButtonUp(0)) 
 		{
-
-			Touch touch = Input.touches [0];
-
+			fingerStart = Vector2.zero;
+			fingerEnd = Vector2.zero;
+			movements.Clear();
 		}
 			
 	}
